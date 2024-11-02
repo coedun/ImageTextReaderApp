1994a8bcbbafd89b74d37694668a9284357e8383
coedun <coedun1@gmail.com>
2024-11-02 15:36:42 +0000

    Including Python Tesseract and OCR for reading text from images

--- app/main.py
+++ app/main.py
@@ -1,19 +1,77 @@
 import pathlib
 import datetime
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import uuid
+import io
+from functools import lru_cache
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile
+)
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
+from PIL import Image
+import pytesseract
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
 
+    class Config:
+            env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+DEBUG=get_settings().debug
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploads"
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
 
 now = datetime.datetime.now()
 @app.get("/", response_class=HTMLResponse) # GET
-def home_view(request: Request):
-        return templates.TemplateResponse("home.html", {"request": request, "abc": now.strftime("%H:%M")}) #path is relative to template dir
+def home_view(request: Request, settings:Settings = Depends(get_settings)):
+    return templates.TemplateResponse("home.html", {"request": request, "abc": now.strftime("%H:%M")}) #path is relative to template dir
 
 @app.post("/") #POST
-def home_detail_view():
-        return {"hello":"world"}
+async def prediction_view(file: UploadFile = File(...), settings:Settings = Depends(get_settings)):
+
+    bytes_str = io.BytesIO(await file.read())
+
+    try:
+        img = Image.open(bytes_str)
+    except:
+         raise HTTPException(detail="Invalid image", status_code=400)
+
+    preds = pytesseract.image_to_string(img)
+    predictions = [x for x in preds.split("\n")]
+
+    return {"results": predictions, "original": preds}
+
+
+# image uploading methods
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file: UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+         raise HTTPException(detail="Invalid endpoint", status_code=400)
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+
+    try:
+        img = Image.open(bytes_str)
+    except:
+         raise HTTPException(detail="Invalid image", status_code=400)
+
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+
+    img.save(dest)
+    return dest
--- test_endpoints.py
+++ test_endpoints.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app, BASE_DIR, UPLOAD_DIR
+import shutil
+import time
+import io
+from PIL import Image, ImageChops
 
 client = TestClient(app)
 
@@ -8,8 +12,49 @@ def test_get_home():
     assert response.status_code == 200
     assert 'text/html' in response.headers['content-type']
 
-def test_post_home():
+def test_invalid_file_error():
     response = client.post("/")
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert 'application/json' in response.headers['content-type']
-    assert response.json() == {"hello":"world"}
+
+
+def test_echo_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/img-echo/", files={"file": open(path, 'rb')})
+        fext = str(path.suffix).replace('.', '')
+        if img is None:
+            assert response.status_code == 400
+        else:
+            # returning a valid image
+            assert response.status_code == 200
+            r_stream = io.BytesIO(response.content)
+            echo_img = Image.open(r_stream)
+            difference = ImageChops.difference(echo_img, img).getbbox()
+
+            assert difference is None
+
+def test_prediction_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/", files={"file": open(path, 'rb')})
+        fext = str(path.suffix).replace('.', '')
+        if img is None:
+            assert response.status_code == 400
+        else:
+            # returning a valid image
+            assert response.status_code == 200
+            data = response.json()
+            print(data)
+            assert len(data.keys()) ==2
+
+    #time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
